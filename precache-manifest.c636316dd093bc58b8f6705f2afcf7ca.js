bee076a84eb5a933b66ec0dcad6a1d7a11600ec8
Dishant Aarak <dishantarak2696@gmail.com>
2020-11-24 12:15:49 +0530

    Updates

---
+++ precache-manifest.c636316dd093bc58b8f6705f2afcf7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a5dfe4270206b6fbbbb030d19682f9",
+    "url": "/greet/index.html"
+  },
+  {
+    "revision": "caf472cceeeb4c4442bd",
+    "url": "/greet/static/css/main.4a7f9812.chunk.css"
+  },
+  {
+    "revision": "d5ace1f9fce3840ccdac",
+    "url": "/greet/static/js/2.9a28ac25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greet/static/js/2.9a28ac25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf472cceeeb4c4442bd",
+    "url": "/greet/static/js/main.20fb4d02.chunk.js"
+  },
+  {
+    "revision": "38839530b0a79bcef9e4",
+    "url": "/greet/static/js/runtime-main.12ee697c.js"
+  }
+]);
